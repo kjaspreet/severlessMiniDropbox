f747db7e0f74b72932f0a27e4de3ea7c7230f618
kjaspreet <jaspreet.mca2011@gmail.com>
2018-03-20 16:40:57 -0800

    commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+Follow below steps:
+1) Create S3 bucket in aws console.
+2) run sls deploy
+
+This command will create a new dynamodb table. Now,
+1) Upload files in your S3 bucket.
+2) Observe dynamodb table.
+3) Delete some files from your bucket.
+4) Again observe dynamo table.
+
---
+++ uploadFile.js
@@ -0,0 +1,31 @@
+'use strict';
+const AWS = require('aws-sdk'); 
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+module.exports.uploadFile = (event, context, callback) => {
+  event.Records.forEach((record) => {
+    const filename = record.s3.object.key;
+    const eventType = record.eventName
+    const eventTime = record.eventTime
+
+    const params = {
+      TableName: 'minidropbox',
+      Item: {
+        id: uuid.v1(),
+        fileName: filename,
+        eventType: eventType,
+        eventTime: eventTime
+      }
+    }
+
+    dynamoDb.put(params, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+
+});
+};
